e338e46d753ee431a1641b4767f2f0dacbc7bb00
jmychalm13 <104534587+jmychalm13@users.noreply.github.com>
2023-01-24 07:07:38 -0600

    Add files via upload

---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactPaginate from 'react-paginate';
+
+
+const Pagination = ({pageNumber, setPageNumber}) => {
+    
+  return <ReactPaginate />
+
+}
+
+export default Pagination
